d1b674d92beb3f6ee0a268d159a74c95e0f2bb68
rd <rd@RD-PC>
2019-10-31 23:59:38 +0900

    Chapter 3 C# 데이터 보관

---
+++ Chapter03/Chapter03/IntegerLiterals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class IntegerLiterals //2진수, 10진수, 16진수 리터럴
+        {
+
+
+
+
+    }
+}
---
+++ Chapter03/Chapter03/IntgralTypes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Chapter03
+{
+    class IntgralTypes //자료 타입
+    {/*
+        static void Main(string[] args)
+        {
+            sbyte a = -10;
+            byte b = 40;
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            short c = -30000;
+            ushort d = 60000;
+
+            Console.WriteLine($"c={c}, d={d}");
+
+            int e  = -1000_0000; //리터럴 입력시 언더바 사용하면 숫자 구분할때 편리하다.
+            uint f = 3_0000_0000;
+
+            Console.WriteLine($"e={e}, f={f}");
+
+            long g = -5000_0000_0000;
+            ulong h = 200_0000_0000_0000_0000;
+
+            Console.WriteLine($"g={g}, h={h}");
+
+
+
+        
+        }
+    */
+
+
+        //WriteLine에서 변수 사용할때 $표시와 "" 안에 {사용변수 }
+        //WriteLine(String, Object, Object, Object, Object)
+
+        //Writes the text representation of the specified objects and variable-length parameter list,
+        //followed by the current line terminator, to the standard output stream using the specified format information.
+
+        /*C++
+         public:
+ static void WriteLine(System::String ^ format, System::Object ^ arg0,
+ System::Object ^ arg1, System::Object ^ arg2, System::Object ^ arg3);/
+    */
+        //https://docs.microsoft.com/en-us/dotnet/api/system.console.writeline?view=netframework-4.8
+    }
+}
