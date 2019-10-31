78b2d5180c68a5390bdd8d11af713db414bfcdc8
rd <rd@RD-PC>
2019-10-31 23:27:16 +0900

    Chapter 1~2 C#코드 구조

---
+++ Chapter01/Chapter01/Method.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter01
+{
+    class Method
+    {
+
+        /* static void Main(String[] args)
+        {
+
+            if (args.Length == 0)
+            {
+
+                Console.WriteLine("사용법 : Method.exe <이름>");
+                return;
+
+
+            }
+           Console. WriteLine("Hello, {0}!", args[0]);
+        }
+
+ */      
+    }
+}
--- Chapter01/Chapter01/Program.cs
+++ Chapter01/Chapter01/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using static System.Console;
 namespace Chapter01
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        //프로그램이 실행되는 곳
+        /*
+         static void Main(string[] args)
+         {
+
+             if (args.Length == 0)
+             {
+                 Console.WriteLine("Hello World!");
+                 return;
+             }
+
+             WriteLine("Hello, {0}!", args[0]);
+         }
+ */
     }
+
+    //static (한정자), void (반환형식) main(매소드), (string[] args) 매게변수
+    //static은 프로그램 구동시점 부터 메모리에 할당된다
+    //String[] args in Java is an array of strings which stores arguments(인수) passed by command line
+    //while starting a program.All the command line arguments are stored in that array.
+
 }
